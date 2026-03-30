ab8371bf8a460d5b666361bacd7b9fd36febba2a
mateus-pereira-ezops <mateus-pereira@ezops.cloud>
2026-03-30 16:29:08 -0300

    remove: alerts from prometheus, as we are using grafana already

--- backend/observability/prometheus/entrypoint.sh
+++ backend/observability/prometheus/entrypoint.sh
@@ -2,7 +2,6 @@
 set -e
 
 aws s3 cp s3://${CONFIGS_BUCKET}/observability/prometheus.yml /etc/prometheus/prometheus.yml
-aws s3 cp s3://${CONFIGS_BUCKET}/observability/alerts.yml /etc/prometheus/alerts.yml
 
 exec /bin/prometheus \
   --config.file=/etc/prometheus/prometheus.yml \
